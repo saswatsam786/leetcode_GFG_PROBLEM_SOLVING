0a0360ef68bf37c3be6578accda32c570b64a6ee
Saswat Samal <77463804+saswatsam786@users.noreply.github.com>
2022-02-07 02:27:59 +0530

    Time: 12 ms (13.01%), Space: 12.2 MB (36.33%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& arr) {
+     
+        int n = arr.size();
+        int gidx=0;
+        for(int i=n-1;i>=1;i--){
+            if(arr[i-1]<arr[i]){
+                gidx=i;
+                break;
+            }
+        }
+        cout<<gidx;
+        if(gidx==0){
+            sort(arr.begin(),arr.end());
+            return;
+        }
+    bool con =false;
+        int index = gidx;
+        for(int i=gidx;i<n;i++){
+            if(arr[gidx-1]<arr[i] and arr[i]<arr[index]){
+                index = i;
+            }
+        }
+        if(!con){
+        swap(arr[gidx-1],arr[index]);
+        }
+        sort(arr.begin()+gidx,arr.end());
+        
+        return;
+        
+        
+        
+        
+    }
+};
